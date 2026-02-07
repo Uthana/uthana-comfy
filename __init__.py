a408811e421a2536ee82c2bc42e8b46421df9e5f
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2026-02-06 16:31:31 -0800

    generated from cookiecutter

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for uthana_nodepack."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Anthony Thibault"""
+__email__ = "t@uthana.com"
+__version__ = "0.0.1"
+
+from .src.uthana_nodepack.nodes import NODE_CLASS_MAPPINGS
+from .src.uthana_nodepack.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
